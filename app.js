0b81ed93db1866b5ebaf8a5c8da408cf5d456a33
mohamed_salah <mohamed.salah@spectro.net>
2022-10-19 09:25:43 +0200

    commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const mqtt = require('mqtt')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const host = 'broker.emqx.io'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const connectUrl = `mqtt://${host}:${port}`
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: 'emqx',
+  password: 'public',
+  reconnectPeriod: 1000,
+})
+
+const topic = '/nodejs/mqtt/mo'
+client.on('connect', () => {
+  console.log('Connected')
+  client.subscribe([topic], () => {
+    console.log(`Subscribe to topic '${topic}'`)
+  })
+  client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+    if (error) {
+      console.error(error)
+    }
+  })
+})
+client.on('message', (topic, payload) => {
+  console.log('Received Message:', topic, payload.toString())
+})
+
+app.get('/mq/:key', (req, res) => {
+    client.publish(topic, `${req.params.key}`, { qos: 0, retain: false }, (error) => {
+        if (error) {
+          console.error(error)
+        }
+      })
+      res.send('Hello World!')
+  });
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('listening on port 5000');
+});
